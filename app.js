987fe85e0513f2387fd1306118a9acaaaae8b29b
HendrywDeyvison <hendryw.deyvison@gmail.com>
2023-12-18 10:33:48 -0300

    feat: insert eslint and prettier config files

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/user/:nome', (req, res) => {
-    res.send({
-        "user": req.params.nome
-    });
+app.get("/user/:nome", (req, res) => {
+  res.send({
+    user: req.params.nome,
+  });
 });
 
 app.listen(8080, () => {
-    console.log('Server app running on port 8080!');
+  console.log("Server app running on port 8080!");
 });
